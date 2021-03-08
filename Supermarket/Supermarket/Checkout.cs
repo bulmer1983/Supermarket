26a8381fb29b8ad84fd65511dab669e3a48b7998
Michael.Bulmer <MBulmer1983@gmail.com>
2021-03-08 17:32:36 +0000

    some tidying up and refactoring

--- Supermarket/Supermarket/Checkout.cs
+++ Supermarket/Supermarket/Checkout.cs
@@ -12,6 +12,9 @@ namespace Supermarket
         private IEnumerable<Item> _items;
         private IEnumerable<SpecialOffer> _specialOffers;
 
+        private decimal GetItemPrice(string sku) => _items.First(s => s.Sku == sku).Price;
+        private bool SkuHasOffer(ScannedSku scannedSku) => _specialOffers.FirstOrDefault(o => o.Sku == scannedSku.Sku) != null;
+
         public Checkout(IEnumerable<Item> items, IEnumerable<SpecialOffer> specialOffers)
         {
             _items = items;
@@ -29,47 +32,58 @@ namespace Supermarket
 
         private decimal GetOfferPrice(ScannedSku scannedSku)
         {
-            var offer = _specialOffers.FirstOrDefault(o => o.Sku == scannedSku.Sku);
-            if (offer is null)
-            {
+           if (!SkuHasOffer(scannedSku))
                 return 0m;
-            }
 
-            var numberOfTimesOfferUsed =  QuantityEligibleForOffer(scannedSku) / offer.Quantity;
+            var offer = _specialOffers.FirstOrDefault(o => o.Sku == scannedSku.Sku);
+
+            var numberOfTimesOfferUsed =  UsageOfOffer(scannedSku);
 
             return numberOfTimesOfferUsed * offer.OfferPrice;
                     
         }
 
-        private decimal GetItemPrice(string sku)
-        {
-            return _items.First(s => s.Sku == sku).Price;
-        }
+    
 
 
         private int QuantityEligibleForOffer(ScannedSku scannedSku)
         {
             var offer = _specialOffers.FirstOrDefault(o => o.Sku == scannedSku.Sku);
-
             if (offer is null)
                 return 0;
-            double s = scannedSku.NumberOfScans / offer.Quantity;
-            return (int)Math.Floor(s) * offer.Quantity;
+
+            return UsageOfOffer(scannedSku) * offer.Quantity;
 
         }
 
+        private int UsageOfOffer(ScannedSku scannedSku) 
+        {
+            var offer = _specialOffers.FirstOrDefault(o => o.Sku == scannedSku.Sku);
+                     
+            double s = scannedSku.NumberOfScans / offer.Quantity;
+
+            return (int)Math.Floor(s);
+        }
+
+
+
         public void ScanItem(string Sku)
         {
-            if (!_items.Any(i => i.Sku == Sku))
-            {
-                throw new ArgumentException("Sku not reconsigned");
-            }
+            Vaidate(Sku);
 
             if (_scans.Exists(s => s.Sku == Sku))
                 _scans.Find(s => s.Sku == Sku).NumberOfScans++;
             else
                 _scans.Add(new ScannedSku(Sku, 1));
 
+        }
+
+        private void Vaidate(string Sku)
+        {
+            if (!_items.Any(i => i.Sku == Sku))
+            {
+                throw new ArgumentException("Sku not reconsigned");
+            }
         }
     }
 }
